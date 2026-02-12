0a914d64af95f18ba7952d7f6389150d2831267e
Timofey Solonin <timofey.solonin@jetbrains.com>
2026-02-12 15:59:04 +0100

    Rework the hasSwiftPMDependenciesFlag into a full model
    This model includes the full task name to run and the magic package name
    that will be used to identify whether the package has already been
    installed
    
    ^KT-84214

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/apple/swiftimport/SwiftImportSetupAction.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/apple/swiftimport/SwiftImportSetupAction.kt
@@ -2134,3 +2134,9 @@ internal abstract class CheckCocoaPodsHasNoSwiftPMDependencies : DefaultTask() {
         }
     }
 }
+
+internal fun Project.swiftPMImportIdeContext() = SwiftPMImportIdeContext(
+    project.hasDirectOrTransitiveSwiftPMDependencies().get(),
+    ("${project.path}:${IntegrateLinkagePackageIntoXcodeProject.TASK_NAME}").replace("::", ":"),
+    SYNTHETIC_IMPORT_TARGET_MAGIC_NAME,
+)
