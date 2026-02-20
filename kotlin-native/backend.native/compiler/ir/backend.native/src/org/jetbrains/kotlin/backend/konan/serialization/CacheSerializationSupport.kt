3e30c784c187e6d77c3bf9c4127c5317c159c9c0
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2026-02-20 13:22:05 +0100

    [Klib] Use var-int encoding for element sizes in .knb files
    This change reduces the size of Klibs by ~5%.
    
    #KT-84451 Fixed

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/serialization/CacheSerializationSupport.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/serialization/CacheSerializationSupport.kt
@@ -406,8 +406,8 @@ internal object ClassFieldsSerializer {
         classFields.forEach {
             idSignatureSerializer.protoIdSignature(it.classSignature)
         }
-        val signatures = IrArrayWriter(protoIdSignatureArray.map { it.toByteArray() }).writeIntoMemory()
-        val signatureStrings = IrStringWriter(protoStringArray).writeIntoMemory()
+        val signatures = IrArrayWriter(protoIdSignatureArray.map { it.toByteArray() }, false).writeIntoMemory()
+        val signatureStrings = IrStringWriter(protoStringArray, false).writeIntoMemory()
         val stringTable = buildStringTable {
             classFields.forEach {
                 +it.file.fqName
@@ -431,7 +431,7 @@ internal object ClassFieldsSerializer {
                 stream.writeInt(field.alignment)
             }
         }
-        return IrArrayWriter(listOf(signatures, signatureStrings, stream.buf)).writeIntoMemory()
+        return IrArrayWriter(listOf(signatures, signatureStrings, stream.buf), false).writeIntoMemory()
     }
 
     fun deserializeTo(data: ByteArray, result: MutableList<SerializedClassFields>) {
