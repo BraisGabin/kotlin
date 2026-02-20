2fd17ccdbf8f46d656fcee2792e1b6722faae5a5
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2026-02-20 10:21:19 +0100

    [KMP Parser] Parse companion blocks
    #KT-84286 Fixed

--- compiler/multiplatform-parsing/common/src/org/jetbrains/kotlin/kmp/parser/utils/KotlinParsing.kt
+++ compiler/multiplatform-parsing/common/src/org/jetbrains/kotlin/kmp/parser/utils/KotlinParsing.kt
@@ -1452,10 +1452,17 @@ internal class KotlinParsing private constructor(builder: SemanticWhitespaceAwar
         }
         val decl = mark()
 
+        val isCompanionBlock =
+            atWithRemap(KtTokens.COMPANION_MODIFIER) && lookahead(1) === KtTokens.LBRACE
+
         val detector = ModifierDetector()
         parseModifierList(detector, emptySyntaxElementTypeSet())
 
-        val declType = parseMemberDeclarationRest(detector)
+        val declType = if (isCompanionBlock) {
+            parseCompanionBlock()
+        } else {
+            parseMemberDeclarationRest(detector)
+        }
 
         if (declType == null) {
             errorWithRecovery("Expecting member declaration", emptySyntaxElementTypeSet())
@@ -1493,6 +1500,10 @@ internal class KotlinParsing private constructor(builder: SemanticWhitespaceAwar
         return declType
     }
 
+    private fun parseCompanionBlock(): SyntaxElementType {
+        parseClassBody()
+        return KtNodeTypes.COMPANION_BLOCK
+    }
     /*
      * secondaryConstructor
      *   : modifiers "constructor" valueParameters (":" constructorDelegationCall)? block
