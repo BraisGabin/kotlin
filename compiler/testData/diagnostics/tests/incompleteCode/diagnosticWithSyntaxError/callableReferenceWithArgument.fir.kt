f5e22a40fd4b990de6173c4dfe12636c5cd7d3ce
Alexey Belkov <alexey.belkov@jetbrains.com>
2026-02-25 14:53:36 +0400

    [Tests] Add tests for callable reference with error argument list
    Currently, we don't build FIR for the argument list and its contents.
    
    KT-68260

---
+++ compiler/testData/diagnostics/tests/incompleteCode/diagnosticWithSyntaxError/callableReferenceWithArgument.fir.kt
@@ -0,0 +1,10 @@
+// RUN_PIPELINE_TILL: FRONTEND
+val lam = { "lam" }
+
+fun foo() {
+    ::lam<!SYNTAX!>(unresolved)<!>
+    ::lam<!SYNTAX!>(::lam)<!>
+    ::lam<!SYNTAX!>(fun() {})<!>
+}
+
+/* GENERATED_FIR_TAGS: callableReference, functionDeclaration, lambdaLiteral, propertyDeclaration, stringLiteral */
