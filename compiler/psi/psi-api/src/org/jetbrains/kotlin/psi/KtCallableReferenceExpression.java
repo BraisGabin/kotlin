c62e57dafce12a81ceb6cec2b7d693fd9044cfd7
Alexey Belkov <alexey.belkov@jetbrains.com>
2026-02-25 16:00:44 +0400

    [FIR] Preserve erroneous argument list in callable reference
    In invalid code like `::foo(args)`, the `(args)` is parsed as a part
    of the `KtCallableReferenceExpression` inside an error element.
    
    We need to preserve this argument list in the FIR tree to enable proper
    PSI->FIR mapping in the IDE.
    
    Summary of changes:
    
    - Add `errorArgumentList` field to `FirCallableReferenceAccess`
    - Add `getErrorValueArgumentList()` method to `KtCallableReferenceExpression`
    - Support it in raw FIR builders and `FirExpressionsResolveTransformer`
    - Update `FirRenderer` to render the erroneous argument list
    
    ^KT-68260 Fixed

--- compiler/psi/psi-api/src/org/jetbrains/kotlin/psi/KtCallableReferenceExpression.java
+++ compiler/psi/psi-api/src/org/jetbrains/kotlin/psi/KtCallableReferenceExpression.java
@@ -7,6 +7,8 @@ package org.jetbrains.kotlin.psi;
 
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiErrorElement;
+import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.kotlin.lexer.KtTokens;
@@ -46,6 +48,23 @@ public class KtCallableReferenceExpression extends KtExpressionImpl implements K
     @Override
     public PsiElement findColonColon() {
         return findChildByType(KtTokens.COLONCOLON);
+    }
+
+    /**
+     * Returns the erroneous value argument list that may be present after the callable reference.
+     * This syntax is invalid: {@code ::foo(args)}.
+     *
+     * @return the erroneous value argument list, or null if not present
+     */
+    @Nullable
+    public KtValueArgumentList getErrorValueArgumentList() {
+        for (PsiErrorElement errorElement : PsiTreeUtil.getChildrenOfTypeAsList(this, PsiErrorElement.class)) {
+            KtValueArgumentList argumentList = PsiTreeUtil.findChildOfType(errorElement, KtValueArgumentList.class);
+            if (argumentList != null) {
+                return argumentList;
+            }
+        }
+        return null;
     }
 
     @Override
